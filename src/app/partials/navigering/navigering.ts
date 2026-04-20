59d52c71fcf17891baf54d9caedd12898e755314
alvastern <alva.stern02@gmail.com>
2026-04-20 17:38:47 +0200

    Genererade tre componenter för element som är samma på alla undersidor, header, nav och footer

---
+++ src/app/partials/navigering/navigering.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigering',
+  imports: [],
+  templateUrl: './navigering.html',
+  styleUrl: './navigering.scss',
+})
+export class Navigering {}
