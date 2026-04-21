b8bd36fd202b27930f6e11ced3fb1c4d7d218b25
alvastern <alva.stern02@gmail.com>
2026-04-21 22:30:59 +0200

    Skapande av funktion för uträkning av centimeter till fot

--- src/app/pages/varden/varden.ts
+++ src/app/pages/varden/varden.ts
@@ -7,3 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './varden.scss',
 })
 export class Varden {}
+
+// Räkna ut fot till meter
+const userNumber = "text";
+
+const formFot = document.getElementById("formFot") as HTMLElement;
+const inputMeter = document.getElementById("inputNumberMeter") as HTMLInputElement;
+
+function cmToFot(cm: number): number {
+  return cm / 30.48;
+}
+
+formFot.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const cm = Number(inputMeter.value);
+  const fot = cmToFot(cm);
+});
+
+// Räkna ut celcius till farenheit
+const formCelcius = document.getElementById("formCelcius") as HTMLElement;
+const inputCelcius = document.getElementById("inputNumberCelcius") as HTMLInputElement;
